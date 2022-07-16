42306a2176e9913fb6d9352d32b5a6b8e367035e
Kureans <leonardtengys@gmail.com>
2022-07-17 01:25:29 +0800

    Connect db schema to server

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
+require('dotenv').config();
 const express = require('express');
 const { Client } = require('pg');
+const { readFileSync } = require('fs');
 const port = 3000;
 
 const app = express();
+const initTables = readFileSync("./db/schema.sql").toString();
+const populateTestData = readFileSync("./db/test_data.sql").toString();
+const initTriggers = readFileSync("./db/triggers.sql").toString();
+const initRoutines = readFileSync("./db/routines.sql").toString();
+const initSql = initTables.concat(initTriggers, initRoutines);
+const client = new Client();
+
+client.connect();
+
+client.query(initSql, (err, res) => {
+    console.log(err, res);
+    client.end();
+})
 
 app.get('/', (req, res) => {
     res.send("Hello World!");
 })
+
 app.listen(port, () => {
     console.log(`Example App Listening on Port ${port}`);
-});
+})
